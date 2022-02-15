7cf45c45bc6fc4a34b2fb4fd7da57703aa33abc7
Shaishav Shah <shaishav.shah04@gmail.com>
2022-02-14 19:02:34 -0700

    Finished main activity ui

--- code/app/src/main/java/com/example/qrcodegame/MainActivity.java
+++ code/app/src/main/java/com/example/qrcodegame/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.qrcodegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.qrcodegame.utils.CurrentUserHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView welcomeText;
+    CurrentUserHelper currentUserHelper = CurrentUserHelper.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        // Bind Texts
+        welcomeText = findViewById(R.id.welcomeText);
+        welcomeText.setText("Welcome " + currentUserHelper.getUsername() +"!");
+
+
     }
 }
