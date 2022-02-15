8864b623b66ff1ab9f7cf36d12d54efbc18999ac
Shaishav Shah <shaishav.shah04@gmail.com>
2022-02-15 15:35:40 -0700

    not done. pushing for pull request

--- code/app/src/main/java/com/example/qrcodegame/MainActivity.java
+++ code/app/src/main/java/com/example/qrcodegame/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.qrcodegame;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.qrcodegame.models.QRCode;
 import com.example.qrcodegame.utils.CurrentUserHelper;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Base64;
@@ -25,8 +42,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView analyzeText;
     TextView resultText;
     ImageButton scanQRButton;
-    String qrCodeContent;
-    int codeWorth;
+
+    Button locationPhotoBtn;
+    Button saveQRtoCloudBtn;
+
+    QRCode currentQRCode;
+    byte[] locationImage;
+
+    final FirebaseStorage storage = FirebaseStorage.getInstance();
+    final CollectionReference userCollectionReference = FirebaseFirestore.getInstance().collection("Users");
+    final CollectionReference qrCollectionReference = FirebaseFirestore.getInstance().collection("Codes");
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
     CurrentUserHelper currentUserHelper = CurrentUserHelper.getInstance();
 
     @Override
@@ -40,14 +68,88 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         analyzeText = findViewById(R.id.analyzeText);
         resultText = findViewById(R.id.codeWorthText);
         scanQRButton = findViewById(R.id.scanQRCodeBtn);
+        locationPhotoBtn = findViewById(R.id.takeLocationBtn);
+        saveQRtoCloudBtn = findViewById(R.id.saveQRtoCloudBtn);
 
         // Update
         welcomeText.setText("Welcome " + currentUserHelper.getUsername() +"!");
         analyzeText.setVisibility(View.INVISIBLE);
         resultText.setVisibility(View.INVISIBLE);
 
+        // Updating listners
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        // Fetching image
+                        Bitmap image = (Bitmap) result.getData().getExtras().get("data");
+                        ByteArrayOutputStream bytesStream = new ByteArrayOutputStream();
+                        image.compress(Bitmap.CompressFormat.JPEG, 100, bytesStream);
+                        locationImage = bytesStream.toByteArray();
+                        // Updating UI
+                        locationPhotoBtn.setText("REMOVE IMAGE");
+                        locationPhotoBtn.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.black, null));
+                    }
+                }
+        );
+
         // Listeners
         scanQRButton.setOnClickListener(this);
+
+        locationPhotoBtn.setOnClickListener(view -> {
+
+            if (locationImage == null) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityResultLauncher.launch(intent);
+                return;
+            }
+
+            locationPhotoBtn.setText("TAKE PHOTO");
+            locationPhotoBtn.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.purple_200, null));
+            Toast.makeText(this, "Image removed!", Toast.LENGTH_SHORT).show();
+        });
+
+        saveQRtoCloudBtn.setOnClickListener(v->{
+            saveCode();
+        });
+    }
+
+    private void saveCode() {
+
+        boolean saveLocation = ((CheckBox) findViewById(R.id.saveLocationCheckBox)).isChecked();
+
+        // Check if QR code already exists
+
+            // If so, update QR code,
+            // Update player
+
+        // Else
+
+            // Create QR Code
+            // Already created
+
+            // Save Image
+            StorageReference imageLocationStorage = storage.getReference().child("images").child(currentQRCode.getId() + ".jpg");
+            imageLocationStorage
+                    .putBytes(locationImage)
+                    .addOnSuccessListener(taskSnapshot -> {
+                        imageLocationStorage.getDownloadUrl().addOnSuccessListener( uri -> {
+                            currentQRCode.setImageUrl(uri.toString());
+                        }).addOnFailureListener(e -> {
+                            System.err.println(e.getMessage());
+                        });
+                    })
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(this, "Image could not be saved!", Toast.LENGTH_SHORT).show();
+                        System.err.println(e.getMessage());
+                    });
+
+            // Save Location
+//            if (saveLocation) {
+//
+//            }
+
+            // Save QR Code
 
     }
 
@@ -66,8 +168,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() != null) {
-                qrCodeContent =  result.getContents();
-                calculateWorth();
+                String qrCodeContent =  result.getContents();
+                calculateWorth(qrCodeContent);
                 return;
             } else {
                 Toast.makeText(this, "Scanning cancelled!", Toast.LENGTH_SHORT).show();
@@ -77,7 +179,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void calculateWorth() {
+    public void calculateWorth(String qrCodeContent) {
 
         if (qrCodeContent == null || qrCodeContent.isEmpty()) {
             return;
@@ -106,12 +208,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             // Hashed string here
             final String hashedContent = hashStr.toString();
+
+
             final char[] hashedArray = hashedContent.toCharArray();
 
-            System.out.println("HERE " + hashedContent);
-
             // Calculating String
-            codeWorth = 0;
+            int codeWorth = 0;
             char lastChar = hashedArray[0];
             int count = 0;
             for (char c: hashedArray) {
@@ -129,8 +231,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             resultText.setVisibility(View.VISIBLE);
             resultText.setText("This Hash is worth: " + codeWorth);
 
+            // Updating the QR code Object
+            currentQRCode.setId(hashedContent);
+            currentQRCode.setWorth(codeWorth);
+
+
         } catch (Exception e) {
-            codeWorth = -1;
+            Toast.makeText(this, "Something went wrong!", Toast.LENGTH_SHORT).show();
             System.err.println(e.getMessage());
             return;
         }
--- code/app/src/main/java/com/example/qrcodegame/models/QRCode.java
+++ code/app/src/main/java/com/example/qrcodegame/models/QRCode.java
@@ -1,8 +1,55 @@
 package com.example.qrcodegame.models;
 
+import java.util.ArrayList;
+
 public class QRCode {
 
+    private String id;
+    private int worth;
+    private float[] coordinates = new float[2];
+    private ArrayList<String> players = new ArrayList<>();
+    private String imageUrl;
 
+    public QRCode() {
+    }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getWorth() {
+        return worth;
+    }
+
+    public void setWorth(int worth) {
+        this.worth = worth;
+    }
+
+    public float[] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(float[] coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public ArrayList<String> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<String> players) {
+        this.players = players;
+    }
 }
