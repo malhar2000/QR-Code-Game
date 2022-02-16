b8c6ba2769fa03a537514a90b45f2dbe32dd3809
Shaishav Shah <shaishav.shah04@gmail.com>
2022-02-16 09:52:33 -0700

    tested on phone. reduced the cluttered code

--- code/app/src/main/java/com/example/qrcodegame/MainActivity.java
+++ code/app/src/main/java/com/example/qrcodegame/MainActivity.java
@@ -31,6 +31,7 @@ import android.widget.Toast;
 
 import com.example.qrcodegame.models.QRCode;
 import com.example.qrcodegame.utils.CurrentUserHelper;
+import com.example.qrcodegame.utils.HashHelper;
 import com.example.qrcodegame.utils.LocationHelper;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -57,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Button locationPhotoBtn;
     Button saveQRtoCloudBtn;
+    CheckBox locationToggle;
 
     QRCode currentQRCode;
     byte[] locationImage;
@@ -92,6 +94,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         scanQRButton = findViewById(R.id.scanQRCodeBtn);
         locationPhotoBtn = findViewById(R.id.takeLocationBtn);
         saveQRtoCloudBtn = findViewById(R.id.saveQRtoCloudBtn);
+        locationToggle = findViewById(R.id.saveLocationCheckBox);
 
         // Update
         welcomeText.setText("Welcome " + currentUserHelper.getUsername() + "!");
@@ -143,7 +146,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             saveCode();
         });
 
-        ((CheckBox) findViewById(R.id.saveLocationCheckBox)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        locationToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (b) {
@@ -157,7 +160,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
     };
-
 
 
     private void saveCode() {
@@ -178,7 +180,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void createNewCode() {
 
-        boolean saveLocation = ((CheckBox) findViewById(R.id.saveLocationCheckBox)).isChecked();
+        boolean saveLocation = locationToggle.isChecked();
 
         if (saveLocation && currentQRCode.getCoordinates().size() == 0) {
             System.out.println("RETRY!");
@@ -232,7 +234,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         locationImage = null;
         locationPhotoBtn.setText("TAKE PHOTO");
         locationPhotoBtn.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.purple_200, null));
-        ((CheckBox) findViewById(R.id.saveLocationCheckBox)).setChecked(false);
+        locationToggle.setChecked(false);
     }
 
     private void saveCodeFireStore() {
@@ -273,7 +275,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (result != null) {
             if (result.getContents() != null) {
                 String qrCodeContent = result.getContents();
-                calculateWorth(qrCodeContent);
+                handleHash(qrCodeContent);
             } else {
                 Toast.makeText(this, "Scanning cancelled!", Toast.LENGTH_SHORT).show();
             }
@@ -282,106 +284,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void calculateWorth(String qrCodeContent) {
+    public void handleHash(String qrCodeContent) {
 
-        if (qrCodeContent == null || qrCodeContent.isEmpty()) {
-            return;
-        }
-
-        if (qrCodeContent.startsWith("Account-Transfer=")) {
-            // Transfer account settings
-            return;
-        }
-
-        if (qrCodeContent.startsWith("View-Profile=")) {
-            // View someones profile
-            return;
-        }
-
-        try {
-
-            // calculate sha-256
-            // Citation: https://stackoverflow.com/questions/5531455/how-to-hash-some-string-with-sha256-in-java
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hash = md.digest(qrCodeContent.getBytes(StandardCharsets.UTF_8));
-
-            final StringBuilder hashStr = new StringBuilder(hash.length);
-            for (byte hashByte : hash)
-                hashStr.append(Integer.toHexString(255 & hashByte));
-
-            // Hashed string here
-            final String hashedContent = hashStr.toString();
-
-
-
-
-            final char[] hashedArray = hashedContent.toCharArray();
-
-            // Calculating String
-            char[] codeArray = new char[hashedContent.length()];
-            int[] intCodeArray = new int[hashedContent.length()+1];
-            int comparer = 0;
-            int codeWorth = 0;
-
-            int counter = 0;
-
-            // Copy char by char into array
-            for (int i = 0; i < hashedContent.length(); i++) {
-                codeArray[i] = hashedContent.charAt(i);
-            }
-
-            // hex to int. put this int in the intCodeArray
-            // help from https://stackoverflow.com/questions/26839558/hex-char-to-int-conversion
-            for (int i = 0; i < hashedContent.length(); i++) {
-                if (codeArray[i] >= '0' && codeArray[i] <= '9')
-                    intCodeArray[i] = codeArray[i] - '0';
-                if (codeArray[i] >= 'A' && codeArray[i] <= 'F')
-                    intCodeArray[i] = codeArray[i] - 'A' + 10;
-                if (codeArray[i] >= 'a' && codeArray[i] <= 'f')
-                    intCodeArray[i] = codeArray[i] - 'a' + 10;
-            }
-
-            // used for cases like "1000"; the 16 at the end breaks the counter since its the
-                // end of the hash string. And 16 is out of the range of a hex so its safe to use
-            intCodeArray[hashedContent.length()] = 16;
-
-            comparer = intCodeArray[0];
-            for (int i = 1; i < hashedContent.length() + 1; i++) {
-                if (intCodeArray[i] == comparer) {
-                    counter += 1;
-                }
-                else {
-                    if (counter != 0)
-                    {
-                        if (comparer != 0) {
-                            codeWorth += Math.pow(comparer, counter);
-                        }
-                        else {
-                            codeWorth += Math.pow(20, counter);
-                        }
-                        counter = 0;
-                    }
-
-                    else if (comparer == 0) {
-                        codeWorth += 1;
-                    }
-                    comparer = intCodeArray[i];
-                }
-            }
-
+        int result = HashHelper.handleHash(this, currentQRCode, qrCodeContent);
+        if (result == 1) {
             // Displaying text
             analyzeText.setVisibility(View.VISIBLE);
             resultText.setVisibility(View.VISIBLE);
-            resultText.setText("This Hash is worth: " + codeWorth);
-
-            // Updating the QR code Object
-            currentQRCode.setId(hashedContent);
-            currentQRCode.setWorth(codeWorth);
-
-
-        } catch (Exception e) {
-            Toast.makeText(this, "Something went wrong!", Toast.LENGTH_SHORT).show();
-            System.err.println(e.getMessage());
+            String message = "This Hash is worth: " + currentQRCode.getWorth();
+            resultText.setText(message);
         }
 
     }
