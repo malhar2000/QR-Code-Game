12d0f13afa1d5f72ab6b340cfd67fccf806a226a
Shaishav Shah <shaishav.shah04@gmail.com>
2022-03-13 19:23:15 -0600

    NOT WORKING. Just saving changes to switch branches

--- code/app/src/test/java/com/example/qrcodegame/QRCodeTest.java
+++ code/app/src/test/java/com/example/qrcodegame/QRCodeTest.java
@@ -1,33 +1,34 @@
-package com.example.qrcodegame;
-
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-import android.app.Application;
-import android.app.Instrumentation;
-
-import androidx.test.platform.app.InstrumentationRegistry;
-
-import com.example.qrcodegame.controllers.QRCodeController;
-
-public class QRCodeTest {
-
-    QRCodeController qrCodeController = new QRCodeController(InstrumentationRegistry.getInstrumentation().getContext(), null, null);
-
-
-    // Test to check hashing properly
-    @Test
-    public void hashing_correct() {
-        String testStr = "HelloWorld";
-        qrCodeController.initNewCode();
-
-    }
-
-    // Test to see if switch profile detected
-
-    // Test to see if view profile detected
-
-    // Test to save code without image and location
-
-
-}
+//package com.example.qrcodegame;
+//
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import android.app.Application;
+//import android.app.Instrumentation;
+//
+//import androidx.test.platform.app.InstrumentationRegistry;
+//
+//import com.example.qrcodegame.controllers.QRCodeController;
+//
+//public class QRCodeTest {
+//
+//
+//
+//    // Test to check hashing properly
+//    @Test
+//    public void hashing_correct() {
+//        QRCodeController qrCodeController = new QRCodeController(InstrumentationRegistry.getInstrumentation().getTargetContext(), null, null);
+//        qrCodeController.initNewCode();
+//        qrCodeController.processHash("HelloWorld");
+//        String hashed = "872e4e50ce9990d8b041330c47c9ddd11bec6b503ae9386a99da8584e9bb12c4";
+//        assertEquals(qrCodeController.getCurrentQrCode().getId(), hashed);
+//    }
+//
+//    // Test to see if switch profile detected
+//
+//    // Test to see if view profile detected
+//
+//    // Test to save code without image and location
+//
+//
+//}
