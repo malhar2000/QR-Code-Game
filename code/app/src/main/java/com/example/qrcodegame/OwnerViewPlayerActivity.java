361704de3bf5c6f4e732c69f92e34dc164d0b493
Harsh Patel <harshpatel0990@gmail.com>
2022-03-20 21:00:55 -0600

    delete button needs work

--- code/app/src/main/java/com/example/qrcodegame/OwnerViewPlayerActivity.java
+++ code/app/src/main/java/com/example/qrcodegame/OwnerViewPlayerActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.renderscript.Element;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.qrcodegame.controllers.FireStoreController;
 import com.example.qrcodegame.models.User;
@@ -49,9 +51,27 @@ public class OwnerViewPlayerActivity extends AppCompatActivity {
             }
             Player_adapter.notifyDataSetChanged();
 
+
+
         });
-    }
-}
+
+//        Button btn_delete;
+//        btn_delete = findViewById(R.id.delete_btn);
+//        btn_delete.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                fireStoreController.getAllUsers().addOnSuccessListener(queryDocumentSnapshots -> {
+//                    for (DocumentSnapshot d : queryDocumentSnapshots.getDocuments()){
+//                        listOfUsers.remove(d.toObject(User.class));
+//                    }
+//
+//            });
+//        }
+//
+//
+//    });
+}}
 //        database.addValueEventListener(new ValueEventListener() {
 //            @Override
 //            public void onDataChange(@NonNull DataSnapshot snapshot) {
